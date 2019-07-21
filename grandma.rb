73eaabcf074c6e9345d97c14601111077fb1ff83
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-21 16:13:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ def speak_to_grandma (phrase)
      "No, Not SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase()
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
  end
